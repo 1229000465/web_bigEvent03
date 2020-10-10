347f0c90a6a72a8888042d69860bd8670bf5fcc9
taotao <1229000465@qq.com>
2020-10-10 16:51:58 +0800

    主页功能完成

--- assets/js/baseAPI.js
+++ assets/js/baseAPI.js
@@ -1,4 +1,21 @@
-var baseURL = "'http://ajax.frontend.itheima.net";
+var baseURL = "http://ajax.frontend.itheima.net";
 $.ajaxPrefilter(function(params) {
     params.url = baseURL + params.url;
+
+    if (params.url.indexOf("/my/") !== -1) {
+        params.headers = {
+            Authorization: localStorage.getItem("token") || ""
+        }
+    }
+
+    //拦截所有响应，判断身份认证信息
+    params.complete = function(res) {
+        // console.log(res);
+        if (res.responseJSON.status === 1 && res.responseJSON.message === "身份认证失败！") {
+            //清空本地token
+            localStorage.removeItem("token");
+            //页面跳转
+            location.href = "/login.html"
+        }
+    }
 })
